94af196ac244fcd77691e9cc03ccb4af4e4b9897
Zorca Orcinus <vs@zorca.org>
2021-09-11 10:35:57 +0300

    add: links to cities pages

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -12,6 +12,11 @@
 <body>
     <div class="mt-16">
         <div class="container mx-auto">
+            <div class="max-w-lg mx-auto text-center mb-8">
+                <a class="underline" href="/weather/bryansk">Брянск</a>
+                <a class="underline" href="/weather/kaluga">Калуга</a>
+                <a class="underline" href="/weather/kazan">Казань</a>
+            </div>
             <div class="max-w-lg mx-auto bg-blue bg-no-repeat bg-cover" style="background-image:url('/images/bg-weather.jpg')">
                 <div class="text-white p-8">
                     <h1 class="text-center text-4xl font-bold">Погода в городе {{ $cityInfo['name'] }}</h1>
